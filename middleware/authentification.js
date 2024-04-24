351adafe280830871cada952df731ee06cf1cbd2
hamda souiden <hamdasouiden12@gmail.com>
2024-04-24 10:04:48 +0200

    new change

--- middleware/authentification.js
+++ middleware/authentification.js
@@ -2,14 +2,17 @@ const jwt = require("jsonwebtoken");
 
 const authentification = (req, res, next)=>{
     const token = req.header("Authorization");
-    if(!token || !token.startWith("Bearer ")){
+    if(!token || !token.startsWith("Bearer ")){
         return res.status(401).send("Authentification Failed: Invalid Token");
     }
 
     try{
         const tokenData = token.split(' ')[1];
         const decodeToken = jwt.verify(tokenData, process.env.JWT_SECRET);
-        req.userId = decodeToken._id;
+        req.user ={
+            _id: decodeToken._id, 
+            isAdmin: decodeToken.isAdmin
+        }
         next();
     }catch(error){
         res.status(400).send(error.message);
