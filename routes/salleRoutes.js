41c0b6d039619f2ad38f541d860bc3d480e1b44a
hamda souiden <hamdasouiden12@gmail.com>
2024-04-24 11:04:18 +0200

    new changes

--- routes/salleRoutes.js
+++ routes/salleRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const salleReunion = require("../controllers/salleReunion");
+const auth = require("../middleware/authentification");
 
 //All Meeting Rooms
-router.get("/all", salleReunion.getAllRooms);
+router.get("/", auth, salleReunion.getAllRooms);
+
+//Meeting Room By Id
+router.get("/:id", auth, salleReunion.getRoomById);
+
+//Create Meeting Room
+router.post("/", auth, salleReunion.createRoom);
+
+//Update Meeting Room
+router.put("/:id", auth, salleReunion.updateRoom);
+
+//Delete Meeting Room
+router.delete("/:id", auth, salleReunion.deleteRoom);
 
 
 module.exports = router;
